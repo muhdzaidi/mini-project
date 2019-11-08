366bb8ca2f567b805e20dbe4bb2262397ed46e24
muhdzaidi <muhdzaidi@outlok.com>
2019-11-08 16:09:39 +0700

    beautify singlestory\

--- src/UserStory/SingleStory.js
+++ src/UserStory/SingleStory.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deleteStory } from './Actions/deleteStory'
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class SingleStory extends Component {
 
@@ -11,30 +13,31 @@ class SingleStory extends Component {
     }
 
     render(){
-        console.log(this.props)
-        const { story } = this.props ? (
-            <div className="container">
-                <h4 className="center">{this.props.story.usId} | {this.props.story.owner}</h4>
-                <h6 className="center">{this.props.story.title}</h6>
-                <p className="center">{this.props.story.Questions}</p>
-                <p className="center">{this.props.story.note}</p>
-                
-                <ProgressBar now={this.props.story.progress} label={`${this.props.story.progress}%`}/>
-
-                <div className="center">
-                    <button className="btn grey" onClick = {this.handleClick}>
-                        Delete story
-                    </button>
-                </div> 
-            </div>
-        ) : (
-            <div className="center">Loading story...</div>
-        )
+        const { story } = this.props
         
         return (
-            <div className=" content">
-                { story }
-            </div>
+            {story} ? (
+                <div className="container content content-bg">
+
+                    <Row>
+                        <Col><h4 className="left">{this.props.story.usId} | {this.props.story.owner}</h4></Col>
+                        <Col><h4 className="right">{this.props.story.title}</h4></Col>
+                    </Row>                        
+
+                    <p className="center">{this.props.story.Questions}</p>
+                    <p className="center">{this.props.story.note}</p>
+                    
+                    <ProgressBar now={this.props.story.progress} label={`${this.props.story.progress}%`}/>
+    
+                    <div className="center">
+                        <button className="btn grey" onClick = {this.handleClick}>
+                            Delete story
+                        </button>
+                    </div> 
+                </div>
+            ) : (
+                <div className="center">Loading story...</div>
+            )
         )
     }
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ const initState = {
     stories: [
         {usId: 'US301062', owner:'Thanh', title: 'Add new Tower Ad Slot to SRP', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'60'},
         {usId: 'US271107', owner:'Tuong Anh', title: 'Adobe: DFP Ad Tag for Custom Links', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'23'},
-        {usId: 'US260509', owner:'Duy', title: 'AT Dependency (KBB Advertising) Used Car Landing Page - Add CPO content & CTA to certified inventory (to mitigate CPO landing and research pages going away)', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'98'}
+        {usId: 'US260509', owner:'Duy', title: 'AT Dependency (KBB Advertising) Used Car Landing Page - Add CPO content & CTA to certified inventory (to mitigate CPO landing and research pages going away)', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'83'}
     ]
 }
 
