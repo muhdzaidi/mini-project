b40fc5fd5ca6cddb4c9ae64af177d0b033c6528b
muhdzaidi <muhdzaidi@outlok.com>
2019-11-12 14:36:08 +0700

    Changed inputComponent to single principle

--- src/UserStory/Entries.js
+++ src/UserStory/Entries.js
@@ -6,7 +6,8 @@ import InputComponent from './InputComponent';
 const initialState = {
     label: { date:"Date:", owner:"Owner:", usId:"Story ID:", title:"Title:", question:"Question:", note:"Note:"},
     storyFields: { date:"", owner:"", usId:"", title:"", question:"", note:"" },
-    fieldErrors: { date:"", owner:"", usId:"", title:"" }
+    fieldErrors: { date:"", owner:"", usId:"", title:"", question:"", note:""  },
+    type: { date:"date", text:"text"}
 }
 
 const constraints = {
@@ -61,6 +62,7 @@ class Entries extends Component {
 
     handleInputChange = (event) => {
         const { id, value } = event.target;
+
         this.setState((prevState) => {
             const { storyFields } = prevState;
             return { storyFields: { ...storyFields,  [id]: value}};
@@ -76,8 +78,57 @@ class Entries extends Component {
                 <form id="form" onSubmit= {this.handleSubmit}>
                     
                     <InputComponent 
-                    fields={{...initialState}}
-                    handleInputChange={this.handleInputChange}
+                        label = {this.state.label.date}
+                        error = {this.state.fieldErrors.date}
+                        id = {Object.keys(this.state.storyFields)[0]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.date}
+                        value = {(this.state.storyFields.date)}
+                    />
+
+                    <InputComponent 
+                        label = {this.state.label.owner}
+                        error = {this.state.fieldErrors.owner}
+                        id = {Object.keys(this.state.storyFields)[1]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.text}
+                        value = {(this.state.storyFields.owner)}
+                    />
+
+                    <InputComponent 
+                        label = {this.state.label.usId}
+                        error = {this.state.fieldErrors.usId}
+                        id = {Object.keys(this.state.storyFields)[2]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.text}
+                        value = {(this.state.storyFields.usId)}
+                    />
+
+                    <InputComponent 
+                        label = {this.state.label.title}
+                        error = {this.state.fieldErrors.title}
+                        id = {Object.keys(this.state.storyFields)[3]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.text}
+                        value = {(this.state.storyFields.title)}
+                    />
+
+                    <InputComponent 
+                        label = {this.state.label.question}
+                        error = {this.state.fieldErrors.question}
+                        id = {Object.keys(this.state.storyFields)[4]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.text}
+                        value = {(this.state.storyFields.question)}
+                    />
+
+                    <InputComponent 
+                        label = {this.state.label.note}
+                        error = {this.state.fieldErrors.note}
+                        id = {Object.keys(this.state.storyFields)[5]}
+                        handleInputChange = {this.handleInputChange}
+                        type = {this.state.type.text}
+                        value = {(this.state.storyFields.note)}
                     />
 
                     <button className="btn yellow darken-2">Submit</button>
