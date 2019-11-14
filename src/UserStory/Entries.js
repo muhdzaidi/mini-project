9246a78219cdf0916993de65a6b5b9505d258b1e
muhdzaidi <muhdzaidi@outlok.com>
2019-11-14 17:07:52 +0700

    changes to object metadata, and most functions

--- src/UserStory/Entries.js
+++ src/UserStory/Entries.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react'
+import { set } from 'lodash';
 import { Link } from 'react-router-dom';
 import validate from 'validate.js';
 import InputComponent from './InputComponent';
 
-const initialState = {
-    label: { date:"Date:", owner:"Owner:", usId:"Story ID:", title:"Title:", question:"Question:", note:"Note:"},
-    storyFields: { date:"", owner:"", usId:"", title:"", question:"", note:"" },
-    fieldErrors: { date:"", owner:"", usId:"", title:"", question:"", note:""  },
-    type: { date:"date", text:"text"}
+const fields = {
+    date: {initialState: "", label:"Date:", storyFields:"", fieldErrors:"", type:"date"},
+    owner: {initialState: "", label:"Owner:", storyFields:"", fieldErrors:"", type:"text"},
+    usId: {initialState: "", label:"Story ID:", storyFields:"", fieldErrors:"", type:"text"},
+    title: {initialState: "", label:"Title:", storyFields:"", fieldErrors:"", type:"text"},
+    question: {initialState: "", label:"Question:", storyFields:"", fieldErrors:"", type:"text"},
+    note: {initialState: "", label:"Note:", storyFields:"", fieldErrors:"", type:"text"},
 }
 
+const { initialState, storyFields, fieldErrors } = { ...fields }
+
+
 const constraints = {
-    date: {
+    "date.storyFields": {
         presence: {
             allowEmpty : false
         }
     },
-    owner: {
+    "owner.storyFields": {
         presence: {
             allowEmpty : false
         }
     },
-    usId: {
+    "usId.storyFields": {
         presence: {
             allowEmpty : false
         }
     },
-    title: {
+    "title.storyFields": {
         presence: {
             allowEmpty : false
         }
@@ -36,106 +42,84 @@ const constraints = {
 class Entries extends Component {
     constructor(props){
         super(props)
-        this.state = initialState;
+        this.state = {...fields};
     }
 
     validate() {
-        const error = validate(this.state.storyFields, constraints)
-        if(error){ 
-            this.setState({fieldErrors: error}, () => {
-            });
+        const stateFields = this.state;
+        const error = validate(stateFields, constraints)
+
+        const test = {...stateFields};
+        Object.keys(error).forEach(key => { 
+            const namePair = key.split('.');
+            const fieldName = namePair[0];
+            set(test, [fieldName, 'fieldErrors'], "");
+        })
+        this.setState(test)
+
+        if(error){
+            const newState = {...stateFields};
+            Object.keys(error).forEach(key => { 
+                const namePair = key.split('.');
+                const fieldName = namePair[0];
+                set(newState, [fieldName, 'fieldErrors'], error[key]);
+            })
+            this.setState(newState);
             return false
         }
         return true
     }
+    
 
     handleSubmit = (event) => {
         event.preventDefault()
         const isValid = this.validate()
+
+
         if (isValid) {
             console.log("Data taken, reset field")
-            this.setState(initialState);
+            this.setState(fields);
         } else {
-            console.log(this.state.fieldErrors, "Incomplete form, see error")
+            console.log(fieldErrors, "Incomplete form, see error")
         }
     }
 
     handleInputChange = (event) => {
         const { id, value } = event.target;
-
         this.setState((prevState) => {
-            const { storyFields } = prevState;
-            return { storyFields: { ...storyFields,  [id]: value}};
+            const prevFieldData = prevState[id];
+            return { [id]: { ...prevFieldData, storyFields: value }  };
         });
     }
     
     render () {
+        const stateFields = this.state;
+        
         return (
-             <div className="container">
-             <h4 className="center">Input User Story</h4>
+            <div className="container">
+            <h4 className="center">Input User Story</h4>
+                <div className="input-field center col s6">
+                    <form id="form" onSubmit= {this.handleSubmit}>
 
-            <div className="input-field center col s6">
-                <form id="form" onSubmit= {this.handleSubmit}>
-                    
-                    <InputComponent 
-                        label = {this.state.label.date}
-                        error = {this.state.fieldErrors.date}
-                        id = {Object.keys(this.state.storyFields)[0]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.date}
-                        value = {(this.state.storyFields.date)}
-                    />
+                        {
+                            Object.keys(fields).map(key => { 
+                                const { label, storyFields, fieldErrors, type } = stateFields[key];
+                                return <InputComponent 
+                                    label = {label}
+                                    error = {fieldErrors}
+                                    id = {key}
+                                    handleInputChange = {this.handleInputChange}
+                                    type = {type}
+                                    value = {storyFields}
+                                />
+                            })
 
-                    <InputComponent 
-                        label = {this.state.label.owner}
-                        error = {this.state.fieldErrors.owner}
-                        id = {Object.keys(this.state.storyFields)[1]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.text}
-                        value = {(this.state.storyFields.owner)}
-                    />
-
-                    <InputComponent 
-                        label = {this.state.label.usId}
-                        error = {this.state.fieldErrors.usId}
-                        id = {Object.keys(this.state.storyFields)[2]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.text}
-                        value = {(this.state.storyFields.usId)}
-                    />
-
-                    <InputComponent 
-                        label = {this.state.label.title}
-                        error = {this.state.fieldErrors.title}
-                        id = {Object.keys(this.state.storyFields)[3]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.text}
-                        value = {(this.state.storyFields.title)}
-                    />
-
-                    <InputComponent 
-                        label = {this.state.label.question}
-                        error = {this.state.fieldErrors.question}
-                        id = {Object.keys(this.state.storyFields)[4]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.text}
-                        value = {(this.state.storyFields.question)}
-                    />
-
-                    <InputComponent 
-                        label = {this.state.label.note}
-                        error = {this.state.fieldErrors.note}
-                        id = {Object.keys(this.state.storyFields)[5]}
-                        handleInputChange = {this.handleInputChange}
-                        type = {this.state.type.text}
-                        value = {(this.state.storyFields.note)}
-                    />
-
-                    <button className="btn yellow darken-2">Submit</button>
-
-                </form>
+                        }
+                        
+                        <button className="btn yellow darken-2">Submit</button>
+                    </form>
+                </div>
             </div>
-        </div>
         ) 
     }
 }
