590aea9187b7667295c1d99e5a35de02834a6d55
muhdzaidi <muhdzaidi@outlok.com>
2019-12-06 10:52:06 +0700

    Created new component for iteration bar, implemented logic, changed the data in redux

--- src/UserStory/Home.js
+++ src/UserStory/Home.js
@@ -3,45 +3,80 @@ import { Link } from 'react-router-dom'
 import bee from '../Images/bee.png'
 import { connect } from 'react-redux'
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import Iteration from './Iteration'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Home extends Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {iterationStatus : "iterationWeek1"};
+    }
+
+    getIterationStatus = newIterationStatus => {
+        this.setState((state) => {
+            return { iterationStatus : newIterationStatus};
+        })
+    };
+
     render(){
         const { stories } = this.props;
 
-        const storyList = stories.length ? (
-            stories.map(story => {
-                return (
-                    <div className="post card" key={story.usId}>
-                        <Link to= {'/' + story.usId}>
-                            <div className="card-content">
-                                    <img src={bee} alt="bumblebee"/>
-                                    <span className="card-title black-text">{story.usId} | {story.owner}</span>
-                                <h6>{story.title}</h6>
-                                <ProgressBar now={story.progress} label={`${story.progress}%`} />
-                                
-                            </div>
-                        </Link>
-                    </div>
-                )
-            })
-        ) : (
-            <div className="center">No posts yet</div>
-        )
-
-        return (
+        return(
             <div className="container home content">
-                <h5 className="center">Interation Status</h5>
-                {storyList}
-                <h5 className="center">Color Chart</h5>
+                <Row className="center">
+                    <Col><Iteration getIterationStatus={this.getIterationStatus} onClick={this.props.handleClick} /></Col>
+                    <h5 className="center topBar">Iteration Status</h5>
+                    <Col><Iteration onClick={this.props.handleClick}></Iteration></Col>
+                </Row>
+            {
+
+                Object.entries(stories).map(story =>  {
+                    const currentIteration = this.state.iterationStatus
+                    
+                    console.log("currentIteration", currentIteration)
+
+                    const iterationTitle = story[0]
+                    const iterationContent = story[1]
+                    
+                    const storyList = iterationContent.length ? (
+                        iterationContent.map(story => {
+                            if(iterationTitle === currentIteration) {
+                                return (
+                                    <div className="post card" key={story.usId}>
+                                        <Link to= {'/' + story.usId}>
+                                            <div className="card-content">
+                                                    <img src={bee} alt="bumblebee"/>
+                                                    <span className="card-title black-text">{story.usId} | {story.owner}</span>
+                                                <h6>{story.title}</h6>
+                                                <ProgressBar now={story.progress} label={`${story.progress}%`} />
+                                            </div>
+                                        </Link>
+                                    </div>
+                                )
+                            }
+                        })
+                    ) : (
+                        <div className="center">No stories yet</div>
+                    )
+                    return (
+                        <div className="home content">
+                            {storyList}
+                        </div>
+                    )
+                })
+            }
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+    // let iterationWeek1 = ownProps.match.params.userstory_id
     return {
-        stories: state.stories
+        stories: state
     }
 }
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,59 @@
-const initState = {
-    stories: [
-        {usId: 'US301062', owner:'Thanh', title: 'Add new Tower Ad Slot to SRP', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'60'},
-        {usId: 'US271107', owner:'Tuong Anh', title: 'Adobe: DFP Ad Tag for Custom Links', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'23'},
-        {usId: 'US260509', owner:'Duy', title: 'AT Dependency (KBB Advertising) Used Car Landing Page - Add CPO content & CTA to certified inventory (to mitigate CPO landing and research pages going away)', Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', note:'lorem ipsum', progress:'83'}
+const iterationStatus = {
+    iterationWeek1 : [
+        {
+            usId: 'US301062', 
+            owner:'Thanh', 
+            title: 'Add new Tower Ad Slot to SRP', 
+            Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', 
+            note:'lorem ipsum', 
+            progress:'60'
+        },
+        {
+            usId: 'US271107', 
+            owner:'Tuong Anh', 
+            title: 'Adobe: DFP Ad Tag for Custom Links', 
+            Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', 
+            note:'lorem ipsum', 
+            progress:'23'
+        },
+        {
+            usId: 'US260509', 
+            owner:'Duy', 
+            title: 'AT Dependency (KBB Advertising) Used Car Landing Page - Add CPO content & CTA to certified inventory (to mitigate CPO landing and research pages going away)', 
+            Questions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat', 
+            note:'lorem ipsum', 
+            progress:'83'
+        }
+    ],
+    iterationWeek2 : [
+        {
+            usId: 'US251221', 
+            owner:'Bob', 
+            title: 'Suspendisse mauris libero', 
+            Questions: 'Suspendisse mauris libero, cursus sed eleifend et, posuere vel mi. Cras hendrerit malesuada dolor sodales sodales. Cras tincidunt aliquam commodo', 
+            note:'Mauris lacinia velit et ultrices congue.', 
+            progress:'44'
+        },
+        {
+            usId: 'US414121', 
+            owner:'Sam', 
+            title: 'Donec lacus dui', 
+            Questions: 'Suspendisse sit amet ultrices massa. Etiam rhoncus, tellus luctus interdum varius, mauris dolor facilisis mauris, sed faucibus orci dui at mauris. Sed dictum sagittis ligula sit amet commodo.', 
+            note:'onec feugiat ex ac orci aliquet, quis porta lectus fringilla. Fusce ornare viverra tincidunt. Curabitur id lectus ut odio gravida volutpat', 
+            progress:'13'
+        },
+        {
+            usId: 'US214214', 
+            owner:'Jackson', 
+            title: 'Beget dignissim leo dapibus', 
+            Questions: 'Nunc et sodales tortor. Curabitur nec gravida orci. Etiam eu sem ligula. Praesent et rhoncus leo. Fusce id nisi ut odio suscipit malesuada. Fusce commodo mauris odio, eu gravida purus viverra vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.', 
+            note:'Vivamus vehicula quis quam ut vehicula.', 
+            progress:'89'
+        }
     ]
 }
 
-const rootReducer = (state = initState, action) => {
+const rootReducer = (state = iterationStatus, action) => {
     if (action.type === 'DELETE_STORY'){
         let newStories = state.stories.filter(story => {
             return action.id !== story.usId
@@ -20,7 +67,6 @@ const rootReducer = (state = initState, action) => {
             ...state,
             stories: [...state.stories, action.newEntry]
         }
-        
     }
     return state;
 }
